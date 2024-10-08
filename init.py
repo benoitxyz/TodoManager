aec71cddce8115f570be8767dc8e4b950c122e98
benoit <benoit@grousset.xyz>
2024-10-08 23:47:38 +0200

    translate optimaize init function

--- init.py
+++ init.py
@@ -2,22 +2,22 @@ import os
 from config import Config
 
 def init(titre):
-
-    if test_todo_file() == "OK" :
+    test_result = test_todo_file()
+    if test_result == "OK" :
         print('Todo list already initialized')
-    elif test_todo_file() == "WRONG" :
+    elif test_result == "WRONG" :
         print(f'A file with target name {Config.TODO_FILE} already exist in the directory')
     else:
-        # Si le fichier n'existe pas, le créer et y ajouter le titre et le commentaire
+        # If the file don not exist, create one with title and end file comment.
         with open(Config.TODO_FILE, 'w') as f:
             f.write(f"# {titre}\n")
-            f.write("\n")  # Ligne vide pour séparer le titre du commentaire
+            f.write("\n")  # Empty line to separate comment from tasks
             f.write(Config.END_FILE_COMMENT + "\n")
         print(f"Todo file '{Config.TODO_FILE}' had been initialized")
     
 def test_todo_file():
     if os.path.isfile(Config.TODO_FILE):
-        # Vérifie si le commentaire est présent dans le fichier
+        # Check if comment is present in the file
         with open(Config.TODO_FILE, 'r') as f:
             contenu = f.read()
             if Config.END_FILE_COMMENT in contenu:
